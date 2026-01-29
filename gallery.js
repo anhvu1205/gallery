47767e152c06bc1390e53b661be79863ed40a860
anhvu1205 <VuTLA.B23CN948@stu.ptit.edu.vn>
2026-01-29 16:18:02 +0700

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,42 @@
+const galleryItem = document.querySelectorAll('.gallery-image')
+
+const lightbox = document.getElementById('lightbox')
+const lightboxImage = document.getElementById('lightbox-image')
+const closeBtn = document.getElementById('closeBtn')
+const nextBtn = document.getElementById('nextBtn')
+const prevBtn = document.getElementById('prevBtn')
+
+const imageData = Array.from(galleryItem).map(item => ({
+    src: item.src,
+    alt: item.alt
+}))
+let currentIndex = 0;
+
+const caption = document.getElementById('caption');
+function updateLightbox(index) {
+    currentIndex = index;
+    const data = imageData[currentIndex];
+    lightboxImage.src = data.src;
+    caption.textContent = data.alt;
+    lightbox.classList.add('active');
+}
+galleryItem.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        updateLightbox(index);
+    });
+});
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % imageData.length;
+    updateLightbox(currentIndex);
+});
+prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + imageData.length) % imageData.length;
+    updateLightbox(currentIndex);
+});
+closeBtn.addEventListener('click', () => {
+    lightbox.classList.remove('active');
+});
+
+
+
+
